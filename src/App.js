37f028ecade506f7866a963d59b1fc979f68340d
roygim <roygim@gmail.com>
2024-01-20 23:48:29 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React from 'react';
+
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+
+import { Box } from '@mui/material';
 
 function App() {
+  const cacheRtl = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+
   return (
-    <div className="App">
-      App123
-    </div>
+    <Box dir="rtl" className="App">
+      <CacheProvider value={cacheRtl}>
+        123
+      </CacheProvider>
+    </Box>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { he } from "date-fns/locale";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={he}>
+      <App />
+    </LocalizationProvider>
   </React.StrictMode>
 );
